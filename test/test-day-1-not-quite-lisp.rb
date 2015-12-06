0b8baaf4940bc8f66df8dcf0454c8dac3f39e6af
Doug Orleans <dougorleans@gmail.com>
2015-12-06 18:14:10 -0500

    Solve part 2 of day 1.

--- test/test-day-1-not-quite-lisp.rb
+++ test/test-day-1-not-quite-lisp.rb
@@ -2,9 +2,11 @@ require 'test-helper'
 require 'day-1-not-quite-lisp'
 
 class TestDay1NotQuiteLisp < Minitest::Test
-  def test_what_floor
+  def setup
     @subject = Santa.new
+  end
 
+  def test_what_floor
     # (()) and ()() both result in floor 0.
     assert_equal 0, @subject.what_floor('(())')
     assert_equal 0, @subject.what_floor('()()')
@@ -23,5 +25,13 @@ class TestDay1NotQuiteLisp < Minitest::Test
     # ))) and )())()) both result in floor -3.
     assert_equal -3, @subject.what_floor(')))')
     assert_equal -3, @subject.what_floor(')())())')
+  end
+
+  def test_position_of_basement_instruction
+    # ) causes him to enter the basement at character position 1.
+    assert_equal 1, @subject.position_of_basement_instruction(')')
+
+    # ()()) causes him to enter the basement at character position 5.
+    assert_equal 5, @subject.position_of_basement_instruction('()())')
   end
 end
