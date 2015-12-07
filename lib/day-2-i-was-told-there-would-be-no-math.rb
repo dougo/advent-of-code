d4503c33519e3607065d03c8b9035b526779108d
Doug Orleans <dougorleans@gmail.com>
2015-12-06 19:31:44 -0500

    Solve Part 2 of Day 2.

--- lib/day-2-i-was-told-there-would-be-no-math.rb
+++ lib/day-2-i-was-told-there-would-be-no-math.rb
@@ -18,6 +18,25 @@ For example:
 
 All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
 
+--- Part Two ---
+
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length
+they need to order, which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any
+one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect
+bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never
+tell.
+
+For example:
+
+ - A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of
+   ribbon for the bow, for a total of 34 feet.
+ - A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet
+   of ribbon for the bow, for a total of 14 feet.
+
+How many total feet of ribbon should they order?
+
 =end
 
 class Present
@@ -26,17 +45,51 @@ class Present
   end
 
   def wrapping_paper_needed
-    2*@l*@w + 2*@w*@h + 2*@h*@l + [@l*@w, @w*@h, @h*@l].min
+    areas = faces.map &:area
+    2 * areas.reduce(:+) + areas.min
+  end
+
+  def ribbon_needed
+    faces.map(&:perimeter).min + @l * @w * @h
   end
 
   def self.total_wrapping_paper_needed(list_of_dimensions)
-    presents = list_of_dimensions.split.map &Present.method(:new)
-    presents.map(&:wrapping_paper_needed).reduce(:+)
+    presents(list_of_dimensions).map(&:wrapping_paper_needed).reduce(:+)
+  end
+
+  def self.total_ribbon_needed(list_of_dimensions)
+    presents(list_of_dimensions).map(&:ribbon_needed).reduce(:+)
+  end
+
+  private
+
+  def self.presents(list_of_dimensions)
+    list_of_dimensions.split.map &Present.method(:new)
+  end
+
+  def faces
+    [Rectangle.new(@l, @w), Rectangle.new(@w, @h), Rectangle.new(@h, @l)]
+  end
+
+  class Rectangle
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+
+    def area
+      @x * @y
+    end
+
+    def perimeter
+      2 * (@x + @y)
+    end
   end
 end
 
 if defined? DATA
-  puts Present.total_wrapping_paper_needed(DATA.read)
+  list_of_dimensions = DATA.read
+  puts Present.total_wrapping_paper_needed(list_of_dimensions)
+  puts Present.total_ribbon_needed(list_of_dimensions)
 end
 
 __END__
--- test/test-day-2-i-was-told-there-would-be-no-math.rb
+++ test/test-day-2-i-was-told-there-would-be-no-math.rb
@@ -2,21 +2,39 @@ require 'test-helper'
 require 'day-2-i-was-told-there-would-be-no-math'
 
 class TestDay2IWasToldThereWouldBeNoMath < Minitest::Test
-  def test_wrapping_paper_needed
-    # A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square
-    # feet of slack, for a total of 58 square feet.
-    assert_equal 58, Present.new('2x3x4').wrapping_paper_needed
-
-    # A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square
-    # foot of slack, for a total of 43 square feet.
-    assert_equal 43, Present.new('1x1x10').wrapping_paper_needed
-  end
-
-  def test_total_wrapping_paper_needed
-    list_of_dimensions = <<END
+  def setup
+    @list_of_dimensions = <<END
 2x3x4
 1x1x10
 END
-    assert_equal 101, Present.total_wrapping_paper_needed(list_of_dimensions)
+    @dimensions1, @dimensions2 = @list_of_dimensions.split
+  end
+
+  def test_wrapping_paper_needed
+    # A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square
+    # feet of slack, for a total of 58 square feet.
+    assert_equal 58, Present.new(@dimensions1).wrapping_paper_needed
+
+    # A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square
+    # foot of slack, for a total of 43 square feet.
+    assert_equal 43, Present.new(@dimensions2).wrapping_paper_needed
+  end
+
+  def test_total_wrapping_paper_needed
+    assert_equal 101, Present.total_wrapping_paper_needed(@list_of_dimensions)
+  end
+
+  def test_ribbon_needed
+    # A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet
+    # of ribbon for the bow, for a total of 34 feet.
+    assert_equal 34, Present.new(@dimensions1).ribbon_needed
+
+    # A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10
+    # feet of ribbon for the bow, for a total of 14 feet.
+    assert_equal 14, Present.new(@dimensions2).ribbon_needed
+  end
+  
+  def test_total_ribbon_needed
+    assert_equal 48, Present.total_ribbon_needed(@list_of_dimensions)
   end
 end
