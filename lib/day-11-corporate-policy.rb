b56cca9f83801da40ff6eac7530ba25336af3f2a
Doug Orleans <dougorleans@gmail.com>
2015-12-11 01:59:58 -0500

    Solve Day 11.

---
+++ lib/day-11-corporate-policy.rb
@@ -0,0 +1,85 @@
+=begin
+
+--- Day 11: Corporate Policy ---
+
+Santa's previous password expired, and he needs help choosing a new one.
+
+To help him remember his new password after the old one expires, Santa has devised a method of coming up with a
+password based on the previous one. Corporate policy dictates that passwords must be exactly eight lowercase
+letters (for security reasons), so he finds his new password by incrementing his old password string repeatedly
+until it is valid.
+
+Incrementing is just like counting with numbers: xx, xy, xz, ya, yb, and so on. Increase the rightmost letter one
+step; if it was z, it wraps around to a, and repeat with the next letter to the left until one doesn't wrap around.
+
+Unfortunately for Santa, a new Security-Elf recently started, and he has imposed some additional password requirements:
+
+ - Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to
+   xyz. They cannot skip letters; abd doesn't count.
+ - Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are
+   therefore confusing.
+ - Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
+
+For example:
+
+ - hijklmmn meets the first requirement (because it contains the straight hij) but fails the second requirement
+   requirement (because it contains i and l).
+ - abbceffg meets the third requirement (because it repeats bb and ff) but fails the first requirement.
+ - abbcegjk fails the third requirement, because it only has one double letter (bb).
+ - The next password after abcdefgh is abcdffaa.
+ - The next password after ghijklmn is ghjaabcc, because you eventually skip all the passwords that start with
+   ghi..., since i is not allowed.
+
+Given Santa's current password (your puzzle input), what should his next password be?
+
+--- Part Two ---
+
+Santa's password expired again. What's the next one?
+
+=end
+
+require 'set'
+
+class SecurityElf
+  def next_password(password)
+    begin
+      password = password.succ
+      # If the password contains a confusing letter, just replace the first one immediately rather than continue
+      # looping through all the successors. E.g. foobar -> fpaaaa rather than trying foobas, foobat, ...
+      if i = password.index(/[iol]/)
+        l = password.length
+        password[i..-1] = password[i].succ
+        password = password.ljust(l, 'a')
+      end
+    end until legal_password?(password)
+    password
+  end
+
+  def legal_password?(password)
+    has_straight?(password) && has_no_confusing?(password) && has_two_pairs?(password)
+  end
+
+  def has_straight?(password)
+    trigrams = (0..password.length-2).map {|i| password[i..i+2] }
+    trigrams.any? { |t| t[0].succ == t[1] && t[1].succ == t[2] }
+  end
+
+  def has_no_confusing?(password)
+    !(password =~ /[iol]/)
+  end
+
+  def has_two_pairs?(password)
+    password.scan(/(.)\1/).to_set.length > 1
+  end
+end
+
+if defined? DATA
+  elf = SecurityElf.new
+  old_password = DATA.read.chomp
+  new_password = elf.next_password(old_password)
+  puts new_password
+  puts elf.next_password(new_password)
+end
+
+__END__
+cqjxjnds
---
+++ test/test-day-11-corporate-policy.rb
@@ -0,0 +1,35 @@
+require 'test-helper'
+require 'day-11-corporate-policy'
+
+class TestDay11CorporatePolicy < Minitest::Test
+  def setup
+    @subject = SecurityElf.new
+  end
+
+  def test_next_password
+    # hijklmmn meets the first requirement (because it contains the straight hij) but fails the second requirement
+    # requirement (because it contains i and l).
+    assert @subject.has_straight?('hijklmmn')
+    refute @subject.has_no_confusing?('hijklmmn')
+
+    # abbceffg meets the third requirement (because it repeats bb and ff) but fails the first requirement.
+    assert @subject.has_two_pairs?('abbceffg')
+    refute @subject.has_straight?('abbceffg')
+
+    # abbcegjk fails the third requirement, because it only has one double letter (bb).
+    refute @subject.has_straight?('abbcegjk')
+
+    # The next password after abcdefgh is abcdffaa.
+    assert_equal 'abcdffaa', @subject.next_password('abcdefgh')
+
+    # The next password after ghijklmn is ghjaabcc, because you eventually skip all the passwords that start with
+    # ghi..., since i is not allowed.
+    assert_equal 'ghjaabcc', @subject.next_password('ghijklmn')
+
+
+    # Make sure the old password isn't just returned if it's already valid!
+    assert_equal 'ghjbbcdd', @subject.next_password('ghjaabcc')
+
+    refute @subject.has_two_pairs?('aaaa'), 'Pairs must be different!'
+  end
+end
