d0d7bf3eff749c299a49aa85a8bf7380e5c13d3d
Doug Orleans <dougorleans@gmail.com>
2023-12-08 14:15:48 -0500

    Day 8, part 2. This accidentally works, shrug.

--- lib/2023/day-8-haunted-wasteland.rb
+++ lib/2023/day-8-haunted-wasteland.rb
@@ -40,6 +40,50 @@ ZZZ = (ZZZ, ZZZ)
 
 Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
 
+--- Part Two ---
+
+The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel follow the
+instructions, but you've barely left your starting position. It's going to take significantly more steps to escape!
+
+What if the map isn't for people - what if the map is for ghosts? Are ghosts even bound by the laws of spacetime?
+Only one way to find out.
+
+After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names
+ending in A is equal to the number ending in Z! If you were a ghost, you'd probably just start at every node that
+ends with A and follow all of the paths at the same time until they all simultaneously end up at nodes that end
+with Z.
+
+For example:
+
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+
+Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each left/right
+instruction, use that instruction to simultaneously navigate away from both nodes you're currently on. Repeat this
+process until all of the nodes you're currently on end with Z. (If only some of the nodes you're on end with Z,
+they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+
+Step 0: You are at 11A and 22A.
+Step 1: You choose all of the left paths, leading you to 11B and 22B.
+Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+Step 5: You choose all of the left paths, leading you to 11B and 22C.
+Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+
+So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that
+end with Z?
+
 =end
 
 class HauntedWasteland
@@ -47,21 +91,68 @@ class HauntedWasteland
     @left_right, _, *network_lines = text.lines(chomp: true)
     @network = network_lines.map do |line|
       line =~ /(...) = \((...), (...)\)/
-      [$1, [$2, $3]]
+      [$1, { "L" => $2, "R" => $3 }]
     end.to_h
   end
 
-  def steps_required
-    @left_right.chars.cycle.reduce(['AAA', 0]) do |node_and_steps, instruction|
-      node, steps = node_and_steps
-      return steps if node == 'ZZZ'
-      [@network[node]["LR".index(instruction)], steps + 1]
+  def next_index(index)
+    (index + 1) % @left_right.length
+  end
+
+  def instruction(index)
+    @left_right[index]
+  end
+
+  def nodes
+    @network.keys
+  end
+
+  def next_node(node, index)
+    @network[node][instruction(index)]
+  end
+
+  State = Struct.new(:node, :index) do
+    def self.start(node = 'AAA')
+      new(node, 0)
     end
+
+    def next(hw)
+      self.class.new(hw.next_node(node, index), hw.next_index(index))
+    end
+
+    def end?
+      node == 'ZZZ'
+    end
+
+    def path_to_end(hw)
+      path = [self]
+      path << path.last.next(hw) until path.last.end?
+      path
+    end
+  end
+
+  def steps_required(state = State.start)
+    state.path_to_end(self).length - 1
+  end
+
+  class GhostState < State
+    def end?
+      node.end_with? 'Z'
+    end
+  end
+
+  def steps_required_for_ghosts
+    starts = nodes.filter_map { GhostState.start(_1) if _1.end_with? 'A' }
+
+    # TODO: this happens to work for the given input, but it really should figure out the cycle lengths properly.
+    starts.map { steps_required(_1) }.reduce(:lcm)
   end
 end
 
 if defined? DATA
-  puts HauntedWasteland.new(DATA.read).steps_required
+  hw = HauntedWasteland.new(DATA.read)
+  puts hw.steps_required
+  puts hw.steps_required_for_ghosts
 end
 
 __END__
--- test/2023/test-day-8-haunted-wasteland.rb
+++ test/2023/test-day-8-haunted-wasteland.rb
@@ -21,10 +21,26 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 END
+    @subject3 = HauntedWasteland.new <<END
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+END
   end
 
   def test_steps_required
     assert_equal 2, @subject.steps_required
     assert_equal 6, @subject2.steps_required
+  end
+
+  def test_steps_required_for_ghosts
+    assert_equal 6, @subject3.steps_required_for_ghosts
   end
 end
