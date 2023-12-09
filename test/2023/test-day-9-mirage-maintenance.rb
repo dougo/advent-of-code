a71cc0f803607fef7ff014944b6c70f64285354d
Doug Orleans <dougorleans@gmail.com>
2023-12-09 00:15:23 -0500

    Day 9 part 1.

---
+++ test/2023/test-day-9-mirage-maintenance.rb
@@ -0,0 +1,20 @@
+require 'test-helper'
+require '2023/day-9-mirage-maintenance'
+
+class TestMirageMaintenance < Minitest::Test
+  def setup
+    @subject = OasisReport.new <<END
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+END
+  end
+
+  def test_extrapolated_values
+    assert_equal [18, 28, 68], @subject.extrapolated_values
+  end
+
+  def test_sum_of_extrapolated_values
+    assert_equal 114, @subject.sum_of_extrapolated_values
+  end
+end
