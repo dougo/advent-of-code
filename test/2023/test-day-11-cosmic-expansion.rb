86f4ba2c9e0d38f40bd1e1db5e296180ba21c6d5
Doug Orleans <dougorleans@gmail.com>
2023-12-11 02:05:47 -0500

    Day 11, part 1.

---
+++ test/2023/test-day-11-cosmic-expansion.rb
@@ -0,0 +1,38 @@
+require 'test-helper'
+require '2023/day-11-cosmic-expansion'
+
+class TestCosmicExpansion < Minitest::Test
+  def setup
+    @subject = SpaceImage.new <<END
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+END
+  end
+
+  def test_parsing
+    assert_equal 9, @subject.galaxies.length
+    assert_equal [3, 7], @subject.empty_rows
+    assert_equal [2, 5, 8], @subject.empty_cols
+  end
+
+  def test_shortest_path_length
+    galaxies = @subject.galaxies
+    assert_equal 9,  @subject.shortest_path_length(galaxies[4], galaxies[8])
+    assert_equal 15, @subject.shortest_path_length(galaxies[0], galaxies[6])
+    assert_equal 17, @subject.shortest_path_length(galaxies[2], galaxies[5])
+    assert_equal 5,  @subject.shortest_path_length(galaxies[7], galaxies[8])
+    assert_equal 5,  @subject.shortest_path_length(galaxies[8], galaxies[7])
+  end
+
+  def test_sum_of_shortest_path_lengths
+    assert_equal 374, @subject.sum_of_shortest_path_lengths
+  end
+end
