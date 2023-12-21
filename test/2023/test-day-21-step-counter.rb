6544503fc37dd1bbbc813e13a1418ac840c15274
Doug Orleans <dougorleans@gmail.com>
2023-12-21 00:38:19 -0500

    Day 21, part 1. Messy, but it works for now.

---
+++ test/2023/test-day-21-step-counter.rb
@@ -0,0 +1,25 @@
+require 'test-helper'
+require '2023/day-21-step-counter'
+
+class TestStepCounter < Minitest::Test
+  def setup
+    @input = <<END
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+END
+    @subject = StepCounter.parse(@input)
+  end
+
+  def test_num_plots_in_64_steps
+    assert_equal 16, @subject.num_plots_in_steps(6)
+  end
+end
