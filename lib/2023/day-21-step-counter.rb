ee44e22f052dffc62607370039f3b6b0410e5b9a
Doug Orleans <dougorleans@gmail.com>
2023-12-22 16:56:15 -0500

    Support infinite grids. Still too slow for more than 100 steps, though.

--- lib/2023/day-21-step-counter.rb
+++ lib/2023/day-21-step-counter.rb
@@ -93,22 +93,94 @@ map.
 
 Starting from the garden plot marked S on your map, how many garden plots could the Elf reach in exactly 64 steps?
 
+--- Part Two ---
+
+The Elf seems confused by your answer until he realizes his mistake: he was reading from a list of his favorite
+numbers that are both perfect squares and perfect cubes, not his step counter.
+
+The actual number of steps he needs to get today is exactly 26501365.
+
+He also points out that the garden plots and rocks are set up so that the map repeats infinitely in every
+direction.
+
+So, if you were to look one additional map-width or map-height out from the edge of the example map above, you
+would find that it keeps repeating:
+
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##..S####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+
+This is just a tiny three-map-by-three-map slice of the inexplicably-infinite farm layout; garden plots and rocks
+repeat as far as you can see. The Elf still starts on the one middle tile marked S, though - every other repeated S
+is replaced with a normal garden plot (.).
+
+Here are the number of reachable garden plots in this new infinite version of the example map for different numbers
+of steps:
+
+In exactly 6 steps, he can still reach 16 garden plots.
+In exactly 10 steps, he can reach any of 50 garden plots.
+In exactly 50 steps, he can reach 1594 garden plots.
+In exactly 100 steps, he can reach 6536 garden plots.
+In exactly 500 steps, he can reach 167004 garden plots.
+In exactly 1000 steps, he can reach 668697 garden plots.
+In exactly 5000 steps, he can reach 16733044 garden plots.
+
+However, the step count the Elf needs is much larger! Starting from the garden plot marked S on your infinite map,
+how many garden plots could the Elf reach in exactly 26501365 steps?
+
 =end
 
 require_relative '../util'
 
 class StepCounter
-  def self.parse(text)
+  def self.parse(text, infinite: false)
     lines = text.lines(chomp: true).map(&:chars)
-    new(Grid.new(lines))
+    new(Grid.new(lines), infinite:)
   end
 
-  def initialize(grid) = @grid = grid
+  def initialize(grid, infinite: false)
+    @grid, @infinite = grid, infinite
+  end
   attr :grid
+  def infinite? = @infinite
 
   def start_pos = grid.each_position.find { grid[_1] == 'S' }
 
-  def empty?(pos) = grid[pos].in?('.S')
+  def plot_at(pos)
+    grid[infinite? ? Position[pos.row % grid.height, pos.col % grid.width] : pos]
+  end
+
+  def empty?(pos) = plot_at(pos).in?(['.', 'S'])
 
   def num_plots_in_steps(steps)
     pos = start_pos
@@ -121,8 +193,7 @@ class StepCounter
       plots_at_steps << frontier
     end
 
-    # TOOD: odd steps, start at 1 -- add a failing test
-    0.step(by: 2, to: steps).sum { plots_at_steps[_1].length }
+    (steps.even? ? 0 : 1).step(by: 2, to: steps).sum { plots_at_steps[_1].length }
   end
 end
 
@@ -130,6 +201,8 @@ if defined? DATA
   input = DATA.read
   obj = StepCounter.parse(input)
   puts obj.num_plots_in_steps(64)
+  obj = StepCounter.parse(input, infinite: true)
+  puts obj.num_plots_in_steps(500)
 end
 
 __END__
--- test/2023/test-day-21-step-counter.rb
+++ test/2023/test-day-21-step-counter.rb
@@ -17,9 +17,25 @@ class TestStepCounter < Minitest::Test
 ...........
 END
     @subject = StepCounter.parse(@input)
+    @subject2 = StepCounter.parse(@input, infinite: true)
   end
 
   def test_num_plots_in_steps
+    assert_equal 6, @subject.num_plots_in_steps(3)
     assert_equal 16, @subject.num_plots_in_steps(6)
+    assert_equal 33, @subject.num_plots_in_steps(10)
+  end
+
+  def test_infinite_grid
+    assert_predicate @subject2, :infinite?
+    assert_equal 6, @subject2.num_plots_in_steps(3)
+    assert_equal 16, @subject2.num_plots_in_steps(6)
+    assert_equal 50, @subject2.num_plots_in_steps(10)
+    assert_equal 1594, @subject2.num_plots_in_steps(50)
+    assert_equal 6536, @subject2.num_plots_in_steps(100)
+    skip
+    assert_equal 167004, @subject2.num_plots_in_steps(500)
+    assert_equal 668697, @subject2.num_plots_in_steps(1000)
+    assert_equal 16733044, @subject2.num_plots_in_steps(5000)
   end
 end
