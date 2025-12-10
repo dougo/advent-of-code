74c77b30849e4e415bb5c4f3c3c3d7954824ecff
Doug Orleans <dougorleans@gmail.com>
2025-12-10 00:42:13 -0500

    Day 10 part 1.

---
+++ test/2025/test-day-10-factory.rb
@@ -0,0 +1,17 @@
+require 'test-helper'
+require '2025/day-10-factory'
+
+class TestFactory < Minitest::Test
+  def setup
+    @input = <<END
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+END
+    @subject = Factory.parse(@input)
+  end
+
+  def test_fewest_button_presses
+    assert_equal 7, @subject.fewest_button_presses
+  end
+end
